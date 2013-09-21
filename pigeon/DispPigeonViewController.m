35f706bbd98d36f085d0897bc664f57525dfd6e8
tendo <tuyo1009@gmail.com>
2013-09-21 15:01:01 +0900

    second commit

--- pigeon/DispPigeonViewController.m
+++ pigeon/DispPigeonViewController.m
@@ -14,7 +14,9 @@
 
 UIImageView *iv_main = NULL;
 NSTimer *tm = nil;
-float count = 0;
+float count = 0;//0.1秒おきのカウンター
+int icon_interval = 30;
+
 
 @implementation DispPigeonViewController
 
@@ -85,27 +87,28 @@ float count = 0;
     //サブメイン終了
     
     
-    //下段ボタン開始(アイコン：http://freeicon.blog.shinobi.jp/psd-vector%20%E3%83%87%E3%83%BC%E3%82%BF/%E5%95%86%E7%94%A8%E7%84%A1%E6%96%99%E3%81%AE%E3%83%A2%E3%83%8E%E3%82%AF%E3%83%AD%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3%E3%80%8Cwpzoom%20developer%20icon%20set%E3%80%8D)
+    //下段ボタン開始(アイコン：http://www.wpzoom.com/wpzoom/new-freebie-wpzoom-developer-icon-set-154-free-icons/)
     CGRect rect_bt;
     int bt_size = 35;
     int bt_left = 10;
     int bt_top = 400;
     UIImageView *iv_bt = nil;
     NSArray *bt_name = [NSArray arrayWithObjects:
-                    @"icon_smile2.png",
-                    @"home3.png",
-                    @"icon_fight2.png",
-                    @"icon_tool.png",
+                    @"man.png",
+                    @"home.png",
+                    @"fight.png",
+                    @"tools.png",
                     nil];
     for (int bt_no = 0;bt_no < 4;bt_no++){
         //位置とサイズの決定
-        rect_bt = CGRectMake(bt_left + bt_no * bt_size,
+        rect_bt = CGRectMake(bt_left + bt_no * (bt_size + icon_interval),
                              bt_top,
                              bt_size,
                              bt_size);
 //        NSLog(@"no = %d, x = %d, y = %d",bt_no, bt_left + bt_no * bt_size, bt_top);
         iv_bt = [[UIImageView alloc]initWithFrame:rect_bt];
         iv_bt.image = [UIImage imageNamed:[bt_name objectAtIndex:bt_no]];
+        iv_bt.tag = bt_no;//タップされた時に判別できるように番号付けしておく
         UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self
                                                                               action:@selector(onTappedIcon:)];
         [iv_bt addGestureRecognizer:tap];
@@ -179,27 +182,62 @@ float count = 0;
     //おせわボタン押下時->一定時間、別のアニメーションを設定
     NSLog(@"onTappedIcon");
     
+    NSLog(@"%d",[(UIGestureRecognizer *)gr view].tag);//タップされたビューの番号
+    
+    NSArray *iconAr = nil;//[[NSMutableArray alloc]init];
+    switch([(UIGestureRecognizer *)gr view].tag){
+    case 0://おせわボタンが選択された時
+            iconAr = [[NSArray alloc] initWithObjects:@"restaurant.png",
+                      @"joystick.png",
+                      @"tree.png",
+                      @"pencil",
+                      @"first-aid-kit.png",
+                      @"bath.png",
+                      @"sleep.png", nil];
+        break;
+        
+    case 1://ホームボタンが押された時
+            iconAr = [[NSArray alloc] initWithObjects:@"restaurant.png",
+                      @"joystick.png",
+                      @"tree.png",
+                      @"pencil",
+                      nil];
+        break;
+        
+    default://
+        
+        break;
+    }
+    
+    
+    
     //サブメインの(x_no,y_no)要素
-    int wid_sub = 50;
-    int hei_sub = 50;
-    for(int y_no = 0; y_no < 2;y_no++){
-        for (int x_no = 0; x_no < 6; x_no++){
-            NSLog(@"x = %d, y = %d", x_no, y_no);
+    int wid_sub = 30;
+    int hei_sub = 25;
+    int icon_no = 0;
+    int max_icon_no = 5;
+    for(int y_no = 0; y_no < 2 && [iconAr count] > icon_no;y_no++){
+        for (int x_no = 0; x_no < max_icon_no && [iconAr count] > icon_no; x_no++){
+            //一行のアイコン個数が最大配置個数(=max_icon_no)以下もしくはアイコンの格納数まで。
+//            NSLog(@"x = %d, y = %d", x_no, y_no);
             CGRect rect_subXY = CGRectMake(
-                                           10 + x_no * wid_sub + 10,
-                                           275 + y_no * hei_sub + 10,
+                                           10 + x_no * (wid_sub + icon_interval),
+                                           275 + y_no * (hei_sub + icon_interval),
                                            wid_sub,
                                            hei_sub);
             UIImageView *iv_subXY = [[UIImageView alloc]initWithFrame:rect_subXY];
-            iv_subXY.image = [UIImage imageNamed:@"sample4.jpg"];
+            iv_subXY.image = [UIImage imageNamed:[iconAr objectAtIndex:icon_no]];
             iv_subXY.userInteractionEnabled = YES;
             [self.view addSubview:iv_subXY];
             iv_subXY.userInteractionEnabled = YES;
-            UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onTappedSubMenu:)];
+            UITapGestureRecognizer *tap =
+                [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                        action:@selector(onTappedSubMenu:)];
             [iv_subXY addGestureRecognizer:tap];
+            icon_no ++;
+            
         }
     }
-    //サブメイン終了
-    
+    //サブメイン終了    
 }
 @end
